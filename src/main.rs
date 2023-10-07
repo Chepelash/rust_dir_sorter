35ba6da70902084a22040a2ca6274bef7c4ad4f5
Anton Chepelev <chepelash@gmail.com>
2023-10-07 10:52:56 +0300

    works with exclusions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use std::{
-    ffi::OsStr,
-    fs,
-    io,
-    path::Path,
-};
+use std::{ffi::OsStr, fs, io, path::Path, thread, time::Duration};
 
 use clap::Parser;
 
@@ -15,7 +10,7 @@ struct Args {
     dir_path: String,
 
     // excluded extentions
-    #[arg(short, long, default_values_t = Vec::<String>::new())]
+    #[arg(short, long, default_values_t = Vec::<String>::new(), value_delimiter=' ')]
     excluded_extentions: Vec<String>,
 }
 
@@ -26,18 +21,27 @@ fn main() {
     let dir = Path::new(&args.dir_path);
     assert!(dir.is_dir(), "Dir path does not exists!");
     // get files in dir
-    for file_path in dir.read_dir().expect("read dir failed") {
-        if let Ok(file_path) = file_path {
-            let fp = file_path.path();
-            let mut er = fp.as_path().extension().and_then(OsStr::to_str);
-            let ups = er.get_or_insert("unspecified");
+    loop {
+        thread::sleep(Duration::from_secs(5));
 
-            let sort_path = dir.join(Path::new(ups));
-            if !sort_path.is_dir() {
-                // create dir
-                // create_dir(sort_path.to_str().unwrap_or("unspecified"));
-                if let io::Result::Err(_) = fs::create_dir(sort_path.clone()) {
-                    continue;
+        'main_loop: for file_path in dir.read_dir().expect("read dir failed") {
+            if let Ok(file_path) = file_path {
+                let fp = file_path.path();
+                if !fp.is_file() {
+                    continue 'main_loop;
+                }
+                let mut er = fp.as_path().extension().and_then(OsStr::to_str);
+                let ups = er.get_or_insert("unspecified");
+
+                if args.excluded_extentions.contains(&ups.to_string()) {
+                    continue 'main_loop;
+                }
+
+                let sort_path = dir.join(Path::new(ups));
+                if !sort_path.is_dir() {
+                    if let io::Result::Err(_) = fs::create_dir(sort_path.clone()) {
+                        continue 'main_loop;
+                    }
                 }
                 // move file
                 let file_name = fp.file_name();
